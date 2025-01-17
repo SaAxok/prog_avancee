d57c5ae2142352a3dea8170500a86aa8657aafb4
SaAxok <grv.kylian@gmail.com>
2025-01-17 12:16:35 +0100

    Calcul de PI en Master/Worker Socket

--- distributedMC_step1_javaSocket/WorkerSocket.java
+++ distributedMC_step1_javaSocket/WorkerSocket.java
@@ -1,46 +1,62 @@
 import java.io.*;
 import java.net.*;
+
 /**
- * Worker is a server. It computes PI by Monte Carlo method and sends 
+ * Worker is a server. It computes PI by Monte Carlo method and sends
  * the result to Master.
  */
 public class WorkerSocket {
-    static int port = 25545; //default port
+    static int port = 25545; // default port
     private static boolean isRunning = true;
-    
+
     /**
-     * compute PI locally by MC and sends the number of points 
-     * inside the disk to Master. 
+     * compute PI locally by MC and sends the number of points
+     * inside the disk to Master.
      */
+
     public static void main(String[] args) throws Exception {
 
-	if (!("".equals(args[0]))) port=Integer.parseInt(args[0]);
-	System.out.println(port);
+        if (!("".equals(args[0])))
+            port = Integer.parseInt(args[0]);
+        System.out.println(port);
         ServerSocket s = new ServerSocket(port);
         System.out.println("Server started on port " + port);
         Socket soc = s.accept();
-	
+
         // BufferedReader bRead for reading message from Master
         BufferedReader bRead = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 
         // PrintWriter pWrite for writing message to Master
         PrintWriter pWrite = new PrintWriter(new BufferedWriter(new OutputStreamWriter(soc.getOutputStream())), true);
-	String str;
+        String str;
         while (isRunning) {
-	    str = bRead.readLine();          // read message from Master
-	    if (!(str.equals("END"))){
-		System.out.println("Server receives totalCount = " +  str);
-		
-		// compute
-		System.out.println("TODO : compute Monte Carlo and send total");
+            str = bRead.readLine(); // read message from Master
+            if (!(str.equals("END"))) {
+                System.out.println("Server receives totalCount = " + str);
 
-	        pWrite.println(str);         // send number of points in quarter of disk
-	    }else{
-		isRunning=false;
-	    }	    
+                // compute
+                int totalCount = Integer.parseInt(str);
+                int successThrows = 0;
+
+                for (int i = 0; i < totalCount; i++) {
+                    double x = Math.random();
+                    double y = Math.random();
+
+                    if (x * x + y * y <= 1) {
+                        successThrows++;
+                    }
+                }
+                String result = String.valueOf(successThrows);
+
+                System.out.println("Computed PI: " + result);
+
+                pWrite.println(result); // send computed PI to Master
+            } else {
+                isRunning = false;
+            }
         }
         bRead.close();
         pWrite.close();
         soc.close();
-   }
+    }
 }
